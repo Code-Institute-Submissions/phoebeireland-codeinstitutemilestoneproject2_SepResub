4e997e8a1e9e6174edb3b4811ee014083236b408
Phoebe Ireland <pireland2016@gmail.com>
2021-04-01 18:22:14 +0100

    outline the general basic js functions needed

---
+++ script.js
@@ -0,0 +1,27 @@
+function clickTotal() {
+
+}
+
+function childTotal() {
+
+}
+
+function scissorTotal() {
+
+}
+
+function stampTotal() {
+
+}
+
+function laserTotal() {
+
+}
+
+function 3DTotal() {
+
+}
+
+function overallTotal() {
+    
+}
