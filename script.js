4b0ce55d1103d966b0f74b7f20dc26f3a56d159b
Phoebe Ireland <pireland2016@gmail.com>
2021-04-06 02:02:29 +0100

    add notes for what new js needs to be written

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ function overallTotal() {
 }
 
 //write a function that will count the clicks on the smaller buttons for the incrementation and assign that value to a variable
+//then write  afunction that will translate that number of clicks into a usable multiplier for the button upgrades
+//add a hover description for all of the buttons to describe what happens when they're clicked
+//figure out how to add cookie retention of data to save score
+//maybe experiment with adding a leaderboard api to compare people's scores?
 
 //this was taken from the Love Maths Project
 function boxTotal() {
@@ -24,7 +28,7 @@ function boxTotal() {
 
 function childTotal() {
     let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = oldScore + ; //write a variable that will contain the upgrade increments
+    document.getElementById("score").innerText = oldScore + (); //write a variable that will contain the upgrade increments
 }
 
 function scissorTotal() {
