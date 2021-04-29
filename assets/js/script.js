6fab81c6c58f53133a5a6376224de0c52560ac30
Phoebe Ireland <pireland2016@gmail.com>
2021-04-29 16:45:50 +0100

    connect email.js to the form and the js from the contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,24 @@ function loadScore() {
 window.onload = (event) => {
   loadScore();
 }
+
+const btn = document.getElementById('info-button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'contact_form';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
